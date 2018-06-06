7636ca086e870447374862245b83c867738fba9c
Matthew Lemon <lemon@matthewlemon.com>
2018-06-06 09:00:38 +0100

    first parameterized test passing

--- tests.py
+++ tests.py
@@ -6,14 +6,21 @@ python_executable = "/home/lemon/.local/share/virtualenvs/tw-dev-qpewB347/bin/py
 task = "tw-dft.py"
 
 
-@pytest.fixture
-def cleanup():
-    yield None
+@pytest.fixture(params=['2010-10-10', '2010-10-11', '2019-05-01'])
+def test_data(request):
+    print('\n--------------------------------------')
+    print(f'fixturename     :   {request.fixturename}')
+    print(f'scope           :   {request.scope}')
+    print(f'function        :   {request.function.__name__}')
+    print(f'cls             :   {request.cls}')
+    print(f'module          :   {request.module.__name__}')
+    print(f'fspath          :   {request.fspath}')
+    yield request.param
     os.unlink("/home/lemon/.task-test/pending.data")
 
 
-def test_basic_inspection_with_date(cleanup):
-    date = "2018-09-10"
+def test_basic_inspection_with_date(test_data):
+    date = test_data
     inspection = "Port of Harwich"
     subprocess.run(
         f'{python_executable} {task} inspection -dt {date} -ds "{inspection}"',
@@ -28,4 +35,4 @@ def test_basic_inspection_with_date(cleanup):
     ).stdout.split(
         "\n"
     )
-    assert "2018-09-10 Port of Harwich" in output[3]
+    assert f"{test_data} Port of Harwich" in output[3]
