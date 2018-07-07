89ae2c35d6b3daa13a02d97f1cede3df8ab27513
Matthew Lemon <lemon@matthewlemon.com>
2018-07-07 07:02:10 +0100

    passing test on getting basic inspection date from database

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,50 +5,49 @@ import sqlite3
 from twdft.env import TWDFT_DATA_DIR
 
 
-@pytest.fixture(params=[
-    "Awful Site Name With Space",
-    "Bad & Badder Site",
-    "Test Site/1",
-])
+@pytest.fixture(
+    params=["Awful Site Name With Space", "Bad & Badder Site", "Test Site/1"]
+)
 def bad_site_names(request):
-    print('\n-------------------------------------')
-    print(f'fixturename     :   {request.fixturename}')
+    print("\n-------------------------------------")
+    print(f"fixturename     :   {request.fixturename}")
     yield request.param
 
 
-
-@pytest.fixture(params=[
-    ('Port of Harwich', '2010-10-10'),
-    ('Port of Felixtowe', '2010-10-11'),
-    ('Port of Leith', '2019-05-01')
-]
+@pytest.fixture(
+    params=[
+        ("Port of Harwich", "2010-10-10"),
+        ("Port of Felixtowe", "2010-10-11"),
+        ("Port of Leith", "2019-05-01"),
+    ]
 )
 def date_location(request):
-    print('\n--------------------------------------')
-    print(f'fixturename     :   {request.fixturename}')
-    print(f'scope           :   {request.scope}')
-    print(f'function        :   {request.function.__name__}')
-    print(f'cls             :   {request.cls}')
-    print(f'module          :   {request.module.__name__}')
-    print(f'fspath          :   {request.fspath}')
+    print("\n--------------------------------------")
+    print(f"fixturename     :   {request.fixturename}")
+    print(f"scope           :   {request.scope}")
+    print(f"function        :   {request.function.__name__}")
+    print(f"cls             :   {request.cls}")
+    print(f"module          :   {request.module.__name__}")
+    print(f"fspath          :   {request.fspath}")
     yield request.param
     os.unlink("/home/lemon/.task-test/pending.data")
 
 
-@pytest.fixture(params=[
-    ('Port of Harwich', '2010-10-10T10:30'),
-    ('Port of Felixtowe', '2010-10-11T10:30'),
-    ('Port of Leith', '2019-05-01T10:30')
-]
+@pytest.fixture(
+    params=[
+        ("Port of Harwich", "2010-10-10T10:30"),
+        ("Port of Felixtowe", "2010-10-11T10:30"),
+        ("Port of Leith", "2019-05-01T10:30"),
+    ]
 )
 def date_time_location(request):
-    print('\n--------------------------------------')
-    print(f'fixturename     :   {request.fixturename}')
-    print(f'scope           :   {request.scope}')
-    print(f'function        :   {request.function.__name__}')
-    print(f'cls             :   {request.cls}')
-    print(f'module          :   {request.module.__name__}')
-    print(f'fspath          :   {request.fspath}')
+    print("\n--------------------------------------")
+    print(f"fixturename     :   {request.fixturename}")
+    print(f"scope           :   {request.scope}")
+    print(f"function        :   {request.function.__name__}")
+    print(f"cls             :   {request.cls}")
+    print(f"module          :   {request.module.__name__}")
+    print(f"fspath          :   {request.fspath}")
     yield request.param
     os.unlink("/home/lemon/.task-test/pending.data")
 
@@ -56,26 +55,22 @@ def date_time_location(request):
 @pytest.fixture
 def date_natural_location():
     yield "20 August 2018"
+
+
 #   we don't unlink here because the error means no data is created
 #   os.unlink("/home/lemon/.task-test/pending.data")
 
 
 @pytest.fixture
 def date_time_natural_location():
-    yield {'date': "20 August 2018", 'time': "10:30am"}
+    yield {"date": "20 August 2018", "time": "10:30am"}
     os.unlink("/home/lemon/.task-test/pending.data")
-
-
 
 
 TEST_DB = os.path.join(TWDFT_DATA_DIR, "test-twdft.db")
 
-INSPECTORS = [
-    "John McClaren",
-    "Kelvin Muclaleik",
-    "Steven Chrosssol",
-    "Aiden Snarlo",
-]
+INSPECTORS = ["John McClaren", "Kelvin Muclaleik", "Steven Chrosssol", "Aiden Snarlo"]
+
 
 @pytest.fixture
 def test_db():
@@ -249,4 +244,15 @@ def test_db():
             ),
         )
         conn.commit()
+
+        c.execute(
+            """INSERT INTO inspection(
+                    site, date, status, time)
+                VALUES (1, "2018-10-10", "forwardlook", "2pm");
+            """
+        )
+        insp_id = c.lastrowid
+        c.execute(f"""INSERT INTO inspector_inspections VALUES (?,?)""", (1, insp_id))
+        conn.commit()
+
     return TEST_DB
--- tests/test_report.py
+++ tests/test_report.py
@@ -10,3 +10,37 @@ def test_get_site_name(test_db):
             """
         ).fetchone()
         assert row[1] == "Macmillian Port"
+
+
+def test_get_inspector_name(test_db):
+    with sqlite3.connect(test_db) as conn:
+        c = conn.cursor()
+        row = c.execute(
+            """
+            SELECT * FROM inspector;
+            """
+        ).fetchone()
+        assert row[0] == 1
+        assert row[1] == "John"
+        assert row[2] == "McClaren"
+
+
+def test_get_inspection_data(test_db):
+    with sqlite3.connect(test_db) as conn:
+        c = conn.cursor()
+        row = c.execute(
+            """
+            SELECT
+                inspection.id,
+                site.name,
+                inspector.first_name,
+                inspector.last_name,
+                inspection.date,
+                inspection.time
+            FROM inspection
+            INNER JOIN site ON inspection.site=site.id
+            INNER JOIN inspector_inspections ON inspection.id=inspector_inspections.inspection
+            INNER JOIN inspector ON inspector.id=inspector_inspections.inspector
+            """
+        ).fetchall()
+        assert row[0] == (1, 'Macmillian Port', 'John', 'McClaren', '2018-10-10', '2pm')
